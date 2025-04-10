eaf0a20294df5f780710b32523bde8ae3dd151bf
futureapocalypse <future-apocalypsee@protonmail.com>
2025-04-10 16:02:47 +0300

    Initial commit

---
+++ Faces/FacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacesApp.swift
+//  Faces
+//
+//  Created by Mihail Verejan on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
