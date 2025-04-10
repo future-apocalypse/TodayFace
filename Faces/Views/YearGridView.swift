9580d956dd9e2fa85e9ab184abcf17373ac64aac
futureapocalypse <future-apocalypsee@protonmail.com>
2025-04-10 20:52:51 +0300

    Update YearGridView.swift
    Some text to see if colors of the fonts will change in white/dark mode

--- Faces/Views/YearGridView.swift
+++ Faces/Views/YearGridView.swift
@@ -14,7 +14,24 @@ struct YearGridView: View {
             //Background
                     Color.AppBackground
                     .ignoresSafeArea()
-
+            
+            //Content
+            VStack {
+                Text("312")
+                    .font(.system(size: 56, design: .serif))
+                    .foregroundColor(.PrimaryText)
+                
+                Text("days ")
+                    .font(.system(size: 24, design: .serif))
+                    .italic()
+                    .foregroundColor(.SecondaryText) +
+                Text("left in 2025")
+                    .font(.system(size: 24, design: .serif))
+                    .foregroundColor(.PrimaryText)
+                Spacer()
+            }
+            
+            
                  
                 }
         
